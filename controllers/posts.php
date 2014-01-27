46c26e2f93a45c9ba88f6e92680aee7bf1e0662e
tanel.kolga <tanel.kolga@khk.ee>
2014-01-27 12:46:22 +0200

    Rename welcome controller to posts

--- controllers/posts.php
+++ controllers/posts.php
@@ -3,7 +3,7 @@
 class posts extends Controller{
 
 	function index(){
-		$this->users = get_all("SELECT * FROM user");
+		$this->posts = get_all("SELECT * FROM post");
 	}
 
     function index_ajax(){
