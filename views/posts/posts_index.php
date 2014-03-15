59661326b005579b641561760c7488590535cd18
Tanel <tanel.kolga@khk.ee>
2014-03-15 16:54:55 +0200

    Remove broken menu items

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,4 +1,4 @@
-post index: <? foreach ($posts as $post): ?>
+<? foreach ($posts as $post): ?>
     <div class="span8">
         <h1><a href="<?= BASE_URL ?>posts/view/<?= $post['post_id'] ?>"><?= $post['post_subject'] ?></a></h1>
 
--- views/tags/tags_index.php
+++ views/tags/tags_index.php
@@ -1,7 +1,7 @@
 <div class="list-group">
     <?foreach ($tags as $tag):?>
         <a
-            href="<?=BASE_URL?>tags/view/<?=$tag['tag_id']?>" class="list-group-item">
+            href="<?=BASE_URL?>tags/view/<?=$tag['tag_name']?>" class="list-group-item">
             <?=$tag['tag_name']?>
         </a>
     <?endforeach?>
